a5d51abef2782bdc64b6d8c59eab3da649f1a509
Thirumal <thiruit2004@gmail.com>
2026-04-03 15:03:40 +0530

    connected monogoDb

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+import connectDb from './DB/Db.js'
 
 const app=express();
 
@@ -7,7 +10,8 @@ app.get('/',(req,res)=>{
     console.log("This is Home page");
     res.send("This is home page");
 })
-
+ connectDb();
 app.listen(3000,()=>{
+   
      console.log(" My server is running in 3000 port");
 })
