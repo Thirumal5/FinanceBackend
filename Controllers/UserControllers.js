13a11ac0b307b9d3ae8e7a4cb7b7dad0d7f932cf
Thirumal <thiruit2004@gmail.com>
2026-04-03 15:56:29 +0530

    added usercontrollers with proper validation

---
+++ Controllers/UserControllers.js
@@ -0,0 +1,33 @@
+import User from '../models/User.js';
+
+
+
+const usercontrollers = async (req, res)=>
+{
+    try {
+        const { name, email, role } = req.body;
+        if (!name || !email || !role) 
+            {
+            return res.status(400).json({message:"All feilds are required"});
+         }
+
+         const finduser=await   User.findOne({email});
+         if(finduser)
+         {
+             return res.status(409).json({success:false,message:"Email is already Registered"});
+         }
+        const user = new User({
+            name,
+            email,
+            role
+        });
+          await user.save();
+
+        res.status(201).json({ succes: true, message: "User account Created Succesful", user });
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export default usercontrollers;
